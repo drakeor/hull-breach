2c33fb03b4e8e0e53e62dc92fb8bed620e41f4ba
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 20:56:22 -0700

    Adding android character

--- src/game.ts
+++ src/game.ts
@@ -9,30 +9,56 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
+        this.load.setPath('assets');
+
+        this.load.spritesheet([
+            { key: 'android', frameConfig: { frameWidth: 32, frameHeight: 48 } },
+            { key: 'injured-character', frameConfig: { frameWidth: 48, frameHeight: 32 } }
+        ]);
+
+        this.load.glsl('stars', 'starfields.glsl.js');
     }
 
     create ()
     {
         this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
+        var frontWalkConfig = {
+            key: 'walkFront',
+            frames: this.anims.generateFrameNumbers('android', { start: 0, end: 3 }),
+            frameRate: 8,
+            repeat: -1,
+            yoyo: false
+        };
+        var rightWalkConfig = {
+            key: 'walkRight',
+            frames: this.anims.generateFrameNumbers('android', { start: 4, end: 5 }),
+            frameRate: 8,
+            repeat: -1,
+            yoyo: false
+        };
+        var backWalkConfig = {
+            key: 'walkBack',
+            frames: this.anims.generateFrameNumbers('android', { start: 6, end: 9 }),
+            frameRate: 8,
+            repeat: -1,
+            yoyo: false
+        };
+        var leftWalkConfig = {
+            key: 'walkLeft',
+            frames: this.anims.generateFrameNumbers('android', { start: 10, end: 11 }),
+            frameRate: 8,
+            repeat: -1,
+            yoyo: false
+        };
+    
+        this.anims.create(frontWalkConfig);
+        this.anims.create(rightWalkConfig);
+        this.anims.create(backWalkConfig);
+        this.anims.create(leftWalkConfig);
+    
+        this.add.sprite(400, 300, 'android').play('walkBack');
 
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
     }
 }
 
